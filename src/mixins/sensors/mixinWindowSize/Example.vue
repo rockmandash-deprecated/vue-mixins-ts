fac08493d76c0e450b84a4f4c51120535a06d4fd
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-01-13 13:49:45 +0800

    fix: mixinWindowSize

---
+++ src/mixins/sensors/mixinWindowSize/Example.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- you can access these property, if you forgot, you can go to component and simply type this.mixin... and you will get all available property -->
+    <div>x: {{ mixinWindowSize.width }}</div>
+    <div>y: {{ mixinWindowSize.height }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { mixins, mixinWindowSize } from '../../../';
+
+const customMixin = mixinWindowSize();
+// or
+// const customMixin = mixinWindowSize({
+//   onResize: (width, height) => {
+//     // a custom callback for you
+//     console.log(width, height);
+//   },
+//   debounce: {
+//     wait: 300, // provide wait option (by lodash)
+//   },
+//   throttle: {
+//     wait: 300,
+//     options: {
+//       // or lodash throttle options
+//       leading: true,
+//       trailing: false,
+//     },
+//   },
+// });
+
+export default mixins(
+  customMixin
+  // you can add multiple mixin provided by 'vue-mixins-ts' here like mixinWindowSize()
+).extend({
+  // write your normal component here
+  mounted() {
+    // you can type this.mixin... and TypeScript will show all available mixin data property.
+    console.log(this.mixinWindowSize.width);
+    console.log(this.mixinWindowSize.height);
+  },
+});
+</script>
--- src/mixins/sensors/mixinWindowSize/index.ts
+++ src/mixins/sensors/mixinWindowSize/index.ts
@@ -1,34 +1,56 @@
 import Vue from 'vue';
+import { mountedRefType, debounceAndThrottleType } from '../../../types';
+import { decideReturnDebounceOrThrottleOrOriginal } from '../../../utils';
 
 type mixinWindowSizeType = {
   onResize?: (width: number, height: number) => void;
+} & debounceAndThrottleType;
+
+const createInitialData = () => ({
+  mixinWindowSize: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+});
+
+const makeUpdateWindowSize = (
+  mountedRef: mountedRefType,
+  options?: mixinWindowSizeType
+) => {
+  function innerUpdateWindowSize(this: any) {
+    if (mountedRef.isMounted) {
+      this.mixinWindowSize.width = window.innerWidth;
+      this.mixinWindowSize.height = window.innerHeight;
+      if (options && options.onResize) {
+        options.onResize(window.innerWidth, window.innerHeight);
+      }
+    }
+  }
+  return decideReturnDebounceOrThrottleOrOriginal(
+    innerUpdateWindowSize,
+    options
+  );
 };
 
-const mixinWindowSize = (options?: mixinWindowSizeType) =>
-  Vue.extend({
-    data() {
-      return {
-        mixinWindowSize: {
-          width: window.innerWidth,
-          height: window.innerHeight,
-        },
-      };
-    },
+const mixinWindowSize = (options?: mixinWindowSizeType) => {
+  const mountedRef = {
+    isMounted: false,
+  };
+
+  return Vue.extend({
+    data: createInitialData,
     mounted() {
+      mountedRef.isMounted = true;
       window.addEventListener('resize', this.__updateWindowSize);
     },
     destroyed() {
+      mountedRef.isMounted = false;
       window.removeEventListener('resize', this.__updateWindowSize);
     },
     methods: {
-      __updateWindowSize() {
-        this.mixinWindowSize.width = window.innerWidth;
-        this.mixinWindowSize.height = window.innerHeight;
-        if (options && options.onResize) {
-          options.onResize(window.innerWidth, window.innerHeight);
-        }
-      },
+      __updateWindowSize: makeUpdateWindowSize(mountedRef, options),
     },
   });
+};
 
 export { mixinWindowSize };
