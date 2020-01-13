d43828a03b08f32739e20c12bf046f45b6df3743
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-01-13 17:25:15 +0800

    update example

---
+++ example/pages/mixinWindowSize.vue
@@ -0,0 +1,14 @@
+<template>
+  <mixin-window-size></mixin-window-size>
+</template>
+
+<script lang="ts">
+import mixinWindowSize from '../../src/mixins/sensors/mixinWindowSize/Example.vue'
+
+import Vue from 'vue'
+export default Vue.extend({
+  components: {
+    mixinWindowSize
+  }
+})
+</script>
