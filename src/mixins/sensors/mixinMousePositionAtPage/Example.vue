3c27aae5f2c0ccec7940960b32aea6e5a6712d77
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-01-13 13:27:21 +0800

    fix: mixinMousePositionAtPage

--- src/mixins/sensors/mixinMousePositionAtPage/Example.vue
+++ src/mixins/sensors/mixinMousePositionAtPage/Example.vue
@@ -1,6 +1,3 @@
-Usage
-
-```html
 <template>
   <div>
     <!-- you can access these property, if you forgot, you can go to component and simply type this.mixin... and you will get all available property -->
@@ -8,39 +5,41 @@ Usage
     <div>y: {{ mixinMousePositionAtPage.y }}</div>
   </div>
 </template>
-```
 
-```ts
-import { mixins, mixinMousePositionAtPage } from 'vue-mixins-ts';
+<script lang="ts">
+import { mixins, mixinMousePositionAtPage } from '../../../';
 
 const customMixin = mixinMousePositionAtPage();
 // or
-const customMixin2 = mixinMousePositionAtPage({
-  onMouseMove: (x, y) => {
-    // a custom callback for you
-    console.log(x, y);
-  },
-  debounce: {
-    wait: 300, // provide wait option (by lodash)
-  },
-  throttle: {
-    wait: 300,
-    options: {
-      // or lodash throttle options
-      leading: true,
-      trailing: false,
-    },
-  },
-});
+// const customMixin = mixinMousePositionAtPage({
+//   onMouseMove: (x, y) => {
+//     // a custom callback for you
+//     console.log(x, y);
+//   },
+//   debounce: {
+//     wait: 300, // provide wait option (by lodash)
+//   },
+//   throttle: {
+//     wait: 300,
+//     options: {
+//       // or lodash throttle options
+//       leading: true,
+//       trailing: false,
+//     },
+//   },
+// });
 
 const MyComponent = mixins(
   customMixin
   // you can add multiple mixin provided by 'vue-mixins-ts' here like mixinWindowSize()
 ).extend({
+  // write your normal component here
   mounted() {
     // you can type this.mixin... and TypeScript will show all available mixin data property.
     console.log(this.mixinMousePositionAtPage.x);
     console.log(this.mixinMousePositionAtPage.y);
   },
 });
-```
+
+export default MyComponent;
+</script>
